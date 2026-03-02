5bee02919268b0e91f96f58f84cf188bb7963b4f
Guetti <guetti.dev@gmail.com>
2026-03-02 19:18:00 -0300

    feat(messages): add new message for Katalina Lopez Muñoz

--- content/messages.ts
+++ content/messages.ts
@@ -20,4 +20,11 @@ export const messages: Message[] = [
     message:
       "Gracias por ser mi amigo, por compartir las risas y los momentos inolvidables. Recordaré siempre tu personalidad única y tu espíritu alegre. Echaré de menos nuestras conversaciones de anime y debates de videojuegos. Tu amistad ha sido un regalo invaluable en mi vida. Descansa en paz, Totita.",
   },
+  {
+    id: "3",
+    author: "Katalina Lopez Muñoz",
+    date: "Febrero 2026",
+    message:
+    "Gonzalito, en este día que conmemoramos tu nacimiento, quisiera expresar la gratitud que siento por haber compartido mi vida contigo. En este momento de tanta oscuridad y tanto dolor, quiero destacar el privilegio que tuvimos de tenerte con nosotros 27 añitos, muchos años de hermosos recuerdos, abrazos apretados, muchas risas, chistes con humor negro, temas de conversación larguísimos y que solo tú tocabas. Sin duda, siempre extrañaremos tu presencia cálida y esos momentos en los que fuimos felices. Gracias por entregarnos tanto amor y gracias a Dios por ponerte en nuestras vidas. Agradezco seguir soñando contigo cada vez que te extraño y saber exactamente lo que dirías cada vez que te recuerdo en los pequeños momentos. Te amo primito y te recuerdo siempre🤍"
+  }
 ];
