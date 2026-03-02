4ceb469c995f232bc46d7c956177aa5663ff663f
Guetti <guetti.dev@gmail.com>
2026-03-02 20:39:48 -0300

    feat(messages): add new messages for Andrea Ortego and update existing entry

--- content/messages.ts
+++ content/messages.ts
@@ -26,5 +26,11 @@ export const messages: Message[] = [
     date: "Febrero 2026",
     message:
     "Gonzalito, en este día que conmemoramos tu nacimiento, quisiera expresar la gratitud que siento por haber compartido mi vida contigo. En este momento de tanta oscuridad y tanto dolor, quiero destacar el privilegio que tuvimos de tenerte con nosotros 27 añitos, muchos años de hermosos recuerdos, abrazos apretados, muchas risas, chistes con humor negro, temas de conversación larguísimos y que solo tú tocabas. Sin duda, siempre extrañaremos tu presencia cálida y esos momentos en los que fuimos felices. Gracias por entregarnos tanto amor y gracias a Dios por ponerte en nuestras vidas. Agradezco seguir soñando contigo cada vez que te extraño y saber exactamente lo que dirías cada vez que te recuerdo en los pequeños momentos. Te amo primito y te recuerdo siempre🤍"
+  },
+  {
+    id: "4",
+    author: "Andrea Ortego",
+    date: "Febrero 2026",
+    message: "Le quiero mandar unas palabras al Tota, ya que no tuve oportunidad de despedirme. Tota, mibrito, fuiste una luz que iluminó muchos caminos a su paso. Me gustaría, y me encantaría decir que tu lejanía no nos dejó sombra, pero la verdad es que hasta la fecha partes de mi camino no logran volver a tener luz. El mundo siguió girando, lamentablemente, sin ti. Pero en mi mundo tú seguirás, por años y años, iluminando. El día que me vaya nos vamos juntos poh. Gracias por estar ahí, y aquí, entregándonos tanto pese a lo que estabas pasando. Eres una persona tan maravillosa que el mundo no estaba listo para ti, tu humanidad, cariño, y tus abrazos eran algo que salía por completo de la norma. Gracias por contarme de ti, por dejarme entrar a tu vida -y ahora a la de tu familia-, por estar siempre conmigo presencial o no presencialmente. Quiero dedicarte cada pequeño logro, cada día logrado, cada pasito dado a ti porque sin ti no seríamos quiénes somos ahora. Besitos y muchas fotos en el WC para ti❤️‍🩹"
   }
 ];
